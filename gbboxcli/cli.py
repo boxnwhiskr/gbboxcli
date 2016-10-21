90b15542f020476278cd2cd97dd7d4b864deba18
Jangsea Park <jangsea.park@gmail.com>
2016-10-21 14:59:15 +0900

    Never show stacktrace to users

--- gbboxcli/cli.py
+++ gbboxcli/cli.py
@@ -4,13 +4,34 @@ import json
 import os
 
 import click
+import requests
 import yaml
 
-from gbboxcli import api
+import gbboxcli.api
 
 
 def main():
-    cli(obj={})
+    try:
+        cli(obj={})
+    except gbboxcli.api.HttpRemoteError as e:
+        error_obj = {
+            'status_code': e.status_code,
+            'error_type': e.error_type,
+            'message': e.message,
+        }
+        print_error(error_obj)
+    except requests.exceptions.ConnectionError as e:
+        error_obj = {
+            'error_type': 'Connection error',
+            'message': str(e),
+        }
+        print_error(error_obj)
+    except Exception as e:
+        error_obj = {
+            'error_type': str(e.__class__),
+            'message': str(e),
+        }
+        print_error(error_obj)
 
 
 @click.group()
@@ -26,18 +47,15 @@ def meta():
 @meta.command(name='register')
 @click.option('--service-id', required=True)
 def meta_register(service_id):
-    gb = get_api()
-    try:
-        res = gb.register_service(service_id)
-        print_res(res)
-    except api.HttpRemoteError as e:
-        print_error(e)
+    api = get_api()
+    res = api.register_service(service_id)
+    print_res(res)
 
 
 @meta.command(name='list')
 def meta_list():
-    gb = get_api()
-    res = gb.list_services()
+    api = get_api()
+    res = api.list_services()
     print_res(res)
 
 
@@ -45,18 +63,18 @@ def meta_list():
 @click.option('--service-id', required=True)
 @click.option('--config-path', required=True)
 def meta_update(service_id, config_path):
-    gb = get_api()
+    api = get_api()
     with open(config_path, 'r') as f:
         config = yaml.load(f)
-        res = gb.update_config(service_id, config)
+        res = api.update_config(service_id, config)
         print_res(res)
 
 
 @meta.command(name='get')
 @click.option('--service-id', required=True)
 def meta_get(service_id):
-    gb = get_api()
-    res = gb.get_config(service_id)
+    api = get_api()
+    res = api.get_config(service_id)
     print_res(res)
 
 
@@ -75,8 +93,8 @@ def route(service_id, exp_ids, tid, uid, forced_arm_ids):
     else:
         forced_arm_ids_parsed = {}
 
-    gb = get_api()
-    res = gb.route(
+    api = get_api()
+    res = api.route(
         service_id,
         exp_ids.split(','),
         tid,
@@ -92,11 +110,11 @@ def route(service_id, exp_ids, tid, uid, forced_arm_ids):
 @click.option('--uid')
 @click.option('--q', required=True)
 def collect(service_id, tid, uid, q):
-    gb = get_api()
+    api = get_api()
     log = {'tid': tid, 'q': q}
     if uid is not None:
         log['uid'] = uid
-    res = gb.process_log(service_id, log)
+    res = api.process_log(service_id, log)
     print_res(res)
 
 
@@ -105,21 +123,21 @@ def collect(service_id, tid, uid, q):
 @click.option('--exp-id')
 @click.option('--arm-id')
 def report(service_id, exp_id, arm_id):
-    gb = get_api()
+    api = get_api()
     if exp_id is None and arm_id is not None:
         raise click.BadOptionUsage('Specify exp-id option')
 
     if arm_id is not None:
-        res = gb.report_arm_perf(service_id, exp_id, arm_id)
+        res = api.report_arm_perf(service_id, exp_id, arm_id)
     elif exp_id is not None:
-        res = gb.report_arm_perfs(service_id, exp_id)
+        res = api.report_arm_perfs(service_id, exp_id)
     else:
-        res = gb.report_all_arm_perfs(service_id)
+        res = api.report_all_arm_perfs(service_id)
     print_res(res)
 
 
 def get_api():
-    return api.API.get_http_api(
+    return gbboxcli.api.API.get_http_api(
         os.environ.get('GB_END_POINT'),
         os.environ.get('GB_SECRET'),
     )
@@ -129,13 +147,8 @@ def print_res(res):
     print(json.dumps(res, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
-def print_error(e):
-    error = {
-        'status_code': e.status_code,
-        'error_type': e.error_type,
-        'message': e.message,
-    }
-    print(json.dumps(error, sort_keys=True, indent=4, separators=(',', ': ')))
+def print_error(error_obj):
+    print(json.dumps(error_obj, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
